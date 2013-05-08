5b3b0f4dc4dc85c0a5c5588a8e0bf329bbde44ad
Stephen Suen <ssuen@mit.edu>
2013-05-08 17:10:22 -0400

    heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,8 @@
 
 puts 'ROLES'
 YAML.load(ENV['ROLES']).each do |role|
-	Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-	puts 'role: ' << role
+  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+  puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
@@ -25,7 +25,7 @@ puts 'user: ' << user2.name
 user2.add_role :user	
 
 # POPULATE INITIAL WEDDING
-Wedding.create()
+Wedding.create(date: '2014-2-14')
 
 # POPULATE INITIAL EVENTS
 
